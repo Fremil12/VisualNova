ddcda306472c8afc96dd9879ed8e4491bae634dd
palmatemark9i <77265996+Pal-Mate-Mark-9i@users.noreply.github.com>
2025-02-10 12:14:12 +0100

    added footer, header and LandingPage component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,52 @@
 <script setup>
+import LandingPage from './components/LandingPage.vue';
 
 </script>
 
 <template>
-  
+  <header>
+    <nav>
+      <img src="" alt="Logo">
+      <ul>
+        <li>
+          Main Page
+        </li>
+        <li>
+          About us
+        </li>
+        <li>
+          Campaign writer
+        </li>
+      </ul>
+      <p>
+        <button>Sign up</button>
+        <button>Sign in</button>
+      </p>
+      <img src="" alt="ProfPic">
+    </nav>
+  </header>
+  <main>
+    <LandingPage />
+  </main>
+  <footer>
+    <p>
+      <strong>Lorem</strong>
+      <strong>Lorem</strong>
+      <strong>Lorem</strong>
+      <img src="" alt="Logo">
+      <strong>Lorem</strong>
+      <strong>Lorem</strong>
+      <strong>Lorem</strong>
+    </p>
+    <div>
+      <img src="" alt="pic1">
+      <img src="" alt="pic2">
+      <img src="" alt="pic3">
+      <img src="" alt="pic4">
+      <img src="" alt="pic5">
+    </div>
+    All rights reserved!
+  </footer>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
